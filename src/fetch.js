b59d3778f721c802fab90d3c66b7c1fa1615e5b4
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-22 16:23:34 +0300

    home

--- src/fetch.js
+++ src/fetch.js
@@ -1,19 +1,43 @@
-export const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-const options = {
-  key: '30084769-91cedc9ad879b4e4c8b5187f4',
-  q: '',
+const API_KEY = '30084769-91cedc9ad879b4e4c8b5187f4';
+const searchParams = new URLSearchParams({
+  key: API_KEY,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
-};
-export const getPhoto = () => {
-  return fetch(BASE_URL, options).then(responce => {
-    if (!responce.ok) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+});
+
+export const BASE_URL = `https://pixabay.com/api/?${searchParams}`;
+
+export async function getPhoto(search) {
+  try {
+    if (!search.trim()) {
+      console.log('no arg!');
+      return;
     }
-    return responce.json();
-  });
-};
+    const response = await axios.get(`${BASE_URL}&q=${search}`);
+    return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
+}
+
+// const options = {
+//   key: '30084769-91cedc9ad879b4e4c8b5187f4',
+//   q: '',
+// image_type: 'photo',
+// orientation: 'horizontal',
+// safesearch: 'true',
+// };
+// export const getPhoto = () => {
+//   return fetch(BASE_URL, options).then(responce => {
+//     if (!responce.ok) {
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     }
+//     return responce.json();
+//   });
+// };
--- src/main.js
+++ src/main.js
@@ -1,27 +1,92 @@
-import axios from 'axios';
-import notiflix from 'notiflix';
-const refs = {
-  serchForm: document.querySelector('#search-form'),
-  input: document.querySelector('[name="searchQuery"]'),
-  btnSerch: document.querySelector('[type="submit"]'),
-  btnLoadMore: document.querySelector('[type="button"]'),
-  gallery: document.querySelector('.gallery'),
-};
+import './css/style.css';
+import { BASE_URL, getPhoto } from './fetch';
+import Notiflix from 'notiflix';
 
-refs.serchForm.addEventListener('submit', onSerch);
+const galleryEl = document.querySelector('.gallery');
+const formEl = document.querySelector('#search-form');
 
-function onSerch(e) {
-  e.preventDefault();
-  const searchQuery = e.currentTarget.elements.searchQuery.value;
-  const options = {
-    key: '30084767-7a1e2118a8f7c64e4377bd167',
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  };
-  const url = `https://pixabay.com/api/?key=30084767-7a1e2118a8f7c64e4377bd167&q=${searchQuery}&image_type=photo&per_page=40&page=1`;
-  fetch(url, options)
-    .then(r => r.json())
-    .then(console.log);
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  clearMarkup(galleryEl);
+
+  const searchValue = event.currentTarget[0].value;
+  mountData(searchValue);
 }
+
+async function mountData(searchValue) {
+  try {
+    const data = await getPhoto(searchValue);
+    if (data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    data.hits.forEach(photo => {
+      createCardMarkup(photo);
+    });
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+function createCardMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<div class="photo-card">
+  <img src=${webformatURL} alt=${tags} loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:</b><span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+  );
+}
+function clearMarkup(element) {
+  element.innerHTML = '';
+}
+
+// const refs = {
+//   serchForm: document.querySelector('#search-form'),
+//   input: document.querySelector('[name="searchQuery"]'),
+//   btnSerch: document.querySelector('[type="submit"]'),
+//   btnLoadMore: document.querySelector('[type="button"]'),
+//   gallery: document.querySelector('.gallery'),
+// };
+
+// refs.serchForm.addEventListener('submit', onSerch);
+
+// function onSerch(e) {
+//   e.preventDefault();
+//   const searchQuery = e.currentTarget.elements.searchQuery.value;
+//   const options = {
+//     key: '30084767-7a1e2118a8f7c64e4377bd167',
+//     q: '',
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: 'true',
+//   };
+//   const url = `https://pixabay.com/api/?key=30084767-7a1e2118a8f7c64e4377bd167&q=${searchQuery}&image_type=photo&per_page=40&page=1`;
+//   fetch(url, options)
+//     .then(r => r.json())
+//     .then(console.log);
+// }
